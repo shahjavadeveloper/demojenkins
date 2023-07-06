7f2dafdd5ba352cf566c28fa2c6cce0ce6e06e83
shahjavadeveloper <shahjavadeveloper@gmail.com>
2023-07-06 22:25:33 +0530

    first commit

---
+++ src/main/java/com/gaia/igcaqr/Igcaqr3Application.java
@@ -0,0 +1,14 @@
+package com.gaia.igcaqr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Igcaqr3Application {
+
+	public static void main(String[] args) {
+		System.out.println("applictaion started.....");
+		SpringApplication.run(Igcaqr3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/gaia/igcaqr/Igcaqr3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gaia.igcaqr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Igcaqr3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
